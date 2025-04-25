fd2a0fbf4a07a738cf845084d74d54122295d5d8
marwin.booleman <mwbooleman@gmail.com>
2025-04-25 16:23:09 +0200

    rest endpoint and swagger config setup

--- src/main/java/com/bank/marwin/gans/BMG/BmgApplication.java
+++ src/main/java/com/bank/marwin/gans/BMG/BmgApplication.java
@@ -1,13 +1,23 @@
 package com.bank.marwin.gans.BMG;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class BmgApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BmgApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BmgApplication.class, args);
+    }
 
+    @Configuration
+    public class SwaggerConfig {
+        @Bean
+        public GroupedOpenApi publicApi() {
+            return GroupedOpenApi.builder().group("public").pathsToMatch("/**").build();
+        }
+    }
 }
---
+++ src/main/java/com/bank/marwin/gans/BMG/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.bank.marwin.gans.BMG.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/thing")
+    public String thing() {
+        return "this hello";
+    }
+}
+
+
