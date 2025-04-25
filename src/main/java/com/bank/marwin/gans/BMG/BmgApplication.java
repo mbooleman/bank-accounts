6c4a5286392bfaef200df0a5a890e94b6772b2fb
marwin.booleman <mwbooleman@gmail.com>
2025-04-25 15:27:23 +0200

    initial springboot starter

---
+++ src/main/java/com/bank/marwin/gans/BMG/BmgApplication.java
@@ -0,0 +1,13 @@
+package com.bank.marwin.gans.BMG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/marwin/gans/BMG/BmgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.marwin.gans.BMG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
