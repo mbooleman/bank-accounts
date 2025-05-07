5a798d0d8f0a8a58e25702065c0985f3575b5058
marwin.booleman <mwbooleman@gmail.com>
2025-05-07 09:45:38 +0200

    bank repo tests

--- api/src/main/java/com/bank/marwin/gans/BMG/repositories/BankAccountRepository.java
+++ api/src/main/java/com/bank/marwin/gans/BMG/repositories/BankAccountRepository.java
@@ -2,6 +2,7 @@ package com.bank.marwin.gans.BMG.repositories;
 
 import com.bank.marwin.gans.BMG.models.BankAccount;
 import com.bank.marwin.gans.BMG.models.IBAN;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,6 +17,7 @@ public interface BankAccountRepository extends JpaRepository<BankAccount, UUID> 
     Optional<BankAccount> findByIban(IBAN iban);
 
     @Modifying(clearAutomatically = true)
-    @Query("update BankAccount account set account.balance = ?2 where id = ?1")
+    @Transactional
+    @Query("update BankAccount a set a.balance = ?2 where a.id = ?1")
     void updateBalance(UUID bankAccountId, Long newBalance);
 }
--- api/src/test/java/com/bank/marwin/gans/BMG/repositories/BankAccountRepositoryTest.java
+++ api/src/test/java/com/bank/marwin/gans/BMG/repositories/BankAccountRepositoryTest.java
@@ -131,4 +131,52 @@ public class BankAccountRepositoryTest {
 
         assertEquals(Optional.of(account), result);
     }
+
+    @Test
+    void updateBalanceSetsTheBalanceToTheNewAmount() {
+        UUID userId = UUID.randomUUID();
+        User user = new User(userId, "marwin", "marwin@place.com", List.of("abc", "bcd"));
+
+        userRepository.save(user);
+
+        IBAN iban = new IBAN("NL12ABCD0123456789");
+
+        UUID accountId = UUID.randomUUID();
+        BankAccount account = new BankAccount(accountId, iban, AccountType.SAVINGS_ACCOUNT, "mijn account", 1234L, user,
+                Currency.getInstance("EUR"));
+
+        bankAccountRepository.save(account);
+
+        Optional<BankAccount> storedAccount = bankAccountRepository.findById(accountId);
+        assertEquals(account.getBalance(), storedAccount.get().getBalance());
+
+        bankAccountRepository.updateBalance(accountId, account.getBalance() - 1233);
+
+        Optional<BankAccount> updatedAccount = bankAccountRepository.findById(accountId);
+        assertEquals(1, updatedAccount.get().getBalance());
+    }
+
+    @Test
+    void updateBalanceWorksIfAmountIsEqual() {
+        UUID userId = UUID.randomUUID();
+        User user = new User(userId, "marwin", "marwin@place.com", List.of("abc", "bcd"));
+
+        userRepository.save(user);
+
+        IBAN iban = new IBAN("NL12ABCD0123456789");
+
+        UUID accountId = UUID.randomUUID();
+        BankAccount account = new BankAccount(accountId, iban, AccountType.SAVINGS_ACCOUNT, "mijn account", 1234L, user,
+                Currency.getInstance("EUR"));
+
+        bankAccountRepository.save(account);
+
+        Optional<BankAccount> storedAccount = bankAccountRepository.findById(accountId);
+        assertEquals(account.getBalance(), storedAccount.get().getBalance());
+
+        bankAccountRepository.updateBalance(accountId, account.getBalance());
+
+        Optional<BankAccount> updatedAccount = bankAccountRepository.findById(accountId);
+        assertEquals(1234L, updatedAccount.get().getBalance());
+    }
 }
