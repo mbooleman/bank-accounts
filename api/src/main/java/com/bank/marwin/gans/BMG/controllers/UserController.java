e74e9078737ff008ac8df19efa1a5d9eee333cef
marwin.booleman <mwbooleman@gmail.com>
2025-05-08 15:15:22 +0200

    basic working setup for opentelemetry micrometer and jaeger with a trace visible

--- api/src/main/java/com/bank/marwin/gans/BMG/controllers/UserController.java
+++ api/src/main/java/com/bank/marwin/gans/BMG/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.bank.marwin.gans.BMG.controllers.dtos.UserResponseDto;
 import com.bank.marwin.gans.BMG.errors.UserNotFoundException;
 import com.bank.marwin.gans.BMG.models.User;
 import com.bank.marwin.gans.BMG.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +16,14 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
 
     @PostMapping("")
     public ResponseEntity<UserResponseDto> createUser(@RequestBody CreateUserDto userDto) {
+        LOGGER.info("user creation request coming in");
         User user = userDto.toDomain();
 
         userService.createUser(user);
@@ -29,6 +33,7 @@ public class UserController {
 
     @GetMapping("")
     public ResponseEntity<UserResponseDto> findUser(@RequestParam UUID userId) {
+        LOGGER.info("find user for id {} request coming in", userId);
 
         User user = userService.findUserById(userId).orElseThrow(() -> new UserNotFoundException(userId));
 
